65c6c863ed90bbb532fccd0a1079e1fa9e5c98af
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-01-29 21:17:40 +0900

    feat(06-06): create map components index file with clean exports

---
+++ src/components/map/index.ts
@@ -0,0 +1,13 @@
+// Map components
+export { MapProvider } from './map-provider';
+export { CafeMap } from './cafe-map';
+export { CafeMarker } from './cafe-marker';
+export { CafeInfoWindow } from './cafe-info-window';
+export { CafeStaticMap } from './cafe-static-map';
+export { MapWithFilters } from './map-with-filters';
+export { CafeMapWrapper } from './cafe-map-wrapper';
+export { MapFiltersPanel } from './map-filters';
+export { RatingSlider } from './rating-slider';
+export { FeatureToggle } from './feature-toggle';
+export { CafeMapSkeleton } from './cafe-map-skeleton';
+export { CafeMapWrapperDynamic } from './cafe-map-dynamic';
