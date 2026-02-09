72d02fda3fe54b10da9ae84fbc3558160b20935b
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-02-09 20:41:17 +0900

    auto: update page.tsx

--- src/app/(main)/for-you/page.tsx
+++ src/app/(main)/for-you/page.tsx
@@ -50,8 +50,8 @@ export default async function ForYouPage() {
   cafes = shuffle(cafes);
 
   return (
-    <div className="min-h-dvh flex flex-col bg-muted/30">
-      <main id="main-content" className="flex-1">
+    <div className="h-dvh flex flex-col bg-black">
+      <main id="main-content" className="flex-1 min-h-0">
         <ForYouClient
           cafes={cafes}
           favoriteIds={favoriteIds}
