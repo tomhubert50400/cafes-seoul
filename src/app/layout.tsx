7fd9dd1ce8068371ed33271697ebae15f9f02d2d
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-02-12 18:58:13 +0900

    auto: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default async function RootLayout({
         </I18nProvider>
         <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAPS_API_KEY}&libraries=clusterer,services&autoload=false`}
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
       </body>
     </html>
