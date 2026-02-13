62933fbe3931cdee0af9ac5aadf8bae5a5990989
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-02-13 17:08:18 +0900

    auto: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -60,7 +60,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(self)',
+            value: 'camera=(self), microphone=(), geolocation=(self)',
           },
         ],
       },
